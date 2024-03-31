215cf92e6dfc9e9d9cf045a0259a4e85195768d6
M Dedi Rudianto <mdedirudianto@gmail.com>
2024-03-31 16:28:47 +0700

    fix: quran corpus path

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,16 @@
 const fse = require('fs-extra');
 
+import path = require('path');
 import { Meta, Quran } from './models/qute';
 
 export * from './models/qute';
 
 export function loadQuran() {
-  const meta = fse.readJsonSync('./quran/meta.json') as Meta;
-  const ar = fse.readJsonSync('./quran/ar.json') as Quran;
-  const id = fse.readJsonSync('./quran/meta.json') as Quran;
+  const dir = path.join(__dirname, '../quran');
+
+  const meta = fse.readJsonSync(path.join(dir, 'meta.json')) as Meta;
+  const ar = fse.readJsonSync(path.join(dir, 'ar.json')) as Quran;
+  const id = fse.readJsonSync(path.join(dir, 'meta.json')) as Quran;
 
   return { meta, ar, id };
 }
